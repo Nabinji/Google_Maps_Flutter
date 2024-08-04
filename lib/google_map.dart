9dffe535a764a477f600c519ca06f2e8f835727a
Nabinji <nabinpaudyal2057@gmail.com>
2024-08-04 11:26:57 +0545

    update code

--- lib/google_map.dart
+++ lib/google_map.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 // Main widget for displaying Google Map
@@ -12,48 +13,92 @@ class GoogleMapFlutter extends StatefulWidget {
 class _GoogleMapFlutterState extends State<GoogleMapFlutter> {
   // Initial location for the map's camera position (latitude and longitude)
   LatLng myCurrentLocation = const LatLng(27.7172, 85.3240);
-
-  // Variable to store the custom marker icon
-  BitmapDescriptor customIcon = BitmapDescriptor.defaultMarker;
-
-  @override
-  void initState() {
-    customMarker(); // Call to function that sets the custom marker
-    super.initState();
-  }
-
-  // Function to set a custom marker icon
-  void customMarker() {
-    // Load the custom marker image from the assets and set it as the marker icon
-    BitmapDescriptor.asset(
-      const ImageConfiguration(),
-      "assets/images/buspark1.png",
-    ).then((icon) {
-      setState(() {
-        customIcon = icon; // Update the state with the custom icon
-      });
-    });
-  }
+  late GoogleMapController googleMapController;
+  Set<Marker> markers = {};
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: GoogleMap(
-        initialCameraPosition:
-            CameraPosition(target: myCurrentLocation, zoom: 14),
-        markers: {
-          Marker(
-            markerId: const MarkerId("Marker Id"),
-            position: myCurrentLocation,
-            draggable: true, // Allow the marker to be draggable
-            infoWindow: const InfoWindow(
-                title: "Bus Park",
-                snippet: "Local bus park area of kathmandu district Nepal."),
-            onDragEnd: (value) {},
-            icon: customIcon, // Icon for the marker (custom icon in this case)
-          ),
+        myLocationButtonEnabled: false,
+
+        markers: markers,
+        // Setting the controller when the map is created
+        onMapCreated: (GoogleMapController controller) {
+          googleMapController = controller;
+        },
+        // Initial camera position of the map
+        initialCameraPosition: CameraPosition(
+          target: myCurrentLocation,
+          zoom: 14,
+        ),
+      ),
+      // Floating action button to get user's current location
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.white,
+        child: const Icon(
+          Icons.my_location,
+          size: 30,
+        ),
+        onPressed: () async {
+          // Getting the current position of the user
+          Position position = await _determinePosition();
+
+          // Animating the camera to the user's current position
+          googleMapController.animateCamera(
+            CameraUpdate.newCameraPosition(
+              CameraPosition(
+                target: LatLng(position.latitude, position.longitude),
+                zoom: 14,
+              ),
+            ),
+          );
+
+          // Clearing existing markers
+          markers.clear();
+          // Adding a new marker at the user's current position
+          markers.add(
+            Marker(
+              markerId: const MarkerId('currentLocation'),
+              position: LatLng(position.latitude, position.longitude),
+            ),
+          );
+
+          // Refreshing the state to update the UI with new markers
+          setState(() {});
         },
       ),
     );
+  }
+
+  // Function to determine the user's current position
+  Future<Position> _determinePosition() async {
+    bool serviceEnabled;
+    LocationPermission permission;
+
+    // Checking if location services are enabled
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      return Future.error('Location services are disabled');
+    }
+
+    // Checking the location permission status
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      // Requesting permission if it is denied
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        return Future.error("Location permission denied");
+      }
+    }
+
+    // Handling the case where permission is permanently denied
+    if (permission == LocationPermission.deniedForever) {
+      return Future.error('Location permissions are permanently denied');
+    }
+
+    // Getting the current position of the user
+    Position position = await Geolocator.getCurrentPosition();
+    return position;
   }
 }
